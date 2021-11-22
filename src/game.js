1e8392e9ec8d61e40c89ec337107e31c4c0c1850
TomTomOni <pewoppet@gmail.com>
2021-11-23 08:00:35 +1100

    Clean slate

--- src/game.js
+++ src/game.js
@@ -1,11 +1,15 @@
+import InputHandler from './input';
+
 export default class Game {
     constructor(w, h) {
         this.w = w;
         this.h = h;
+        new InputHandler(this);
     }
 
     draw(ctx) {
-        ctx.fillStyle = '#000'
-        ctx.fillRect(0, 0, this.w, this.h)
+        ctx.font = '20px Consolas'
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, this.w, this.h);
     }
 }
